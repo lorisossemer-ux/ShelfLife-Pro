cb97c526d94e2d5b7b4ccd6e99e674217a9fe43f
lorisossemer-ux <lorisossemer@gmail.com>
2025-11-08 15:19:16 +0100

    Delete scripts directory

--- scripts/config.js
+++
@@ -1,5 +0,0 @@
-// scripts/config.js
-window.__SUPABASE = {
-  url: "https://zzpcvjbvkvxvjflpcxvr.supabase.co",
-  anonKey: "sb_publishable_HVsmqYrV0C09UJGEIIx7EQ_2LlgkVo9..." // incolla la tua chiave qui
-};
