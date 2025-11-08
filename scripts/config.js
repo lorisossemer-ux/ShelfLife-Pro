6e543f4a17a9601e6d8afc25904a6d1172fae871
lorisossemer-ux <lorisossemer@gmail.com>
2025-11-08 15:18:00 +0100

    Create config.js

---
+++ scripts/config.js
@@ -0,0 +1,5 @@
+// scripts/config.js
+window.__SUPABASE = {
+  url: "https://zzpcvjbvkvxvjflpcxvr.supabase.co",
+  anonKey: "sb_publishable_HVsmqYrV0C09UJGEIIx7EQ_2LlgkVo9..." // incolla la tua chiave qui
+};
